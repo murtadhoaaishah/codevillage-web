7fd90229e9b34b6b4f2bb813726a65ac3222235c
murtadhoaaishah <aishatmurtadho@gmail.com>
2023-01-31 11:13:43 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
           </div>
         </div>
         <div className='bg-img'>
-          <img src="/images/hero_bg (1).png" />
+          <img src="/images/hero_bg.png" />
         </div>
       </section>
 
@@ -35,19 +35,19 @@ function App() {
           <span className='section2-description'> Be a part of an all-inclusive technology hub where <br /> you learn, collaborate with tech creatives and join a <br /> team of support systems offering you the top tools <br /> and frameworks to build solutions and earn big.</span>
         </div>
         <div className='section2-img-div'>
-          <img src="/public/images/cv-office.jpg" alt="" />
+          <img src="/images/cv-office.jpg" alt="" />
         </div>
         <div className='courses-wrapper'>
           <Courses
-            img={<img src='/public/vectors/bookmark-circle.svg' style={{ height: "3rem" }} />}
+            img={<img src='/vectors/bookmark-circle.svg' style={{ height: "3rem" }} />}
             heading='Web & Mobile Development'
             description={'We design and develop tech solutions for SMEs and Government Agency.'} />
           <Courses
-            img={<img src='/public/vectors/tent-circle.svg' style={{ height: "3rem" }} />}
+            img={<img src='/vectors/tent-circle.svg' style={{ height: "3rem" }} />}
             heading={'Bootcamps & Trainings'}
             description={'We’re building the next generation of tech talents.'} />
           <Courses
-            img={<img src='/public/vectors/workspace-circle.svg' style={{ height: "3rem" }} />}
+            img={<img src='/vectors/workspace-circle.svg' style={{ height: "3rem" }} />}
             heading={'Workspaces & Tech Hub'}
             description={'Work and Connect with other Creatives. All in a fun and cozy environment.'}
           />
